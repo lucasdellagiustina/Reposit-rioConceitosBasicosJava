44f2a23a787e53d98db3ed1d17e36ca620f14c93
Lucas Della Giustina <lucasdellagiustina21@gmail.com>
2025-09-02 18:09:50 -0300

    RepositorioConceitosBasicosJava/entradadedados/TiposPrimitivos.java

--- entradadedados/TiposPrimitivos.java
+++ entradadedados/TiposPrimitivos.java
@@ -8,7 +8,7 @@ public class TiposPrimitivos {
         String nomeAluno = teclado.nextLine();
         System.out.print("Digite a nota do aluno: ");
         float nota = teclado.nextFloat();
-        System.out.printf("A nota de %s é %.2f%n", nomeAluno, nota);
+        System.out.printf("A nota de %s é %.2f%n", nomeAluno, nota); // Exemplo: Paulo, 8;50
         teclado.close(); // precisa fechar o scanner para evitar vazamento de memória
     }
 }    
