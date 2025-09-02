c79ada96e6a11e31380734f721242ed1a524ab0d
Lucas Della Giustina <lucasdellagiustina21@gmail.com>
2025-09-02 18:06:08 -0300

    RepositorioConceitosBasicosJava/entradadedados/TiposPrimitivos.java

--- entradadedados/TiposPrimitivos.java
+++ entradadedados/TiposPrimitivos.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class TiposPrimitivos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
         String nomeAluno = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: ");
         float nota = teclado.nextFloat();
         System.out.printf("A nota de %s é %.2f%n", nomeAluno, nota);
         teclado.close(); // precisa fechar o scanner para evitar vazamento de memória
