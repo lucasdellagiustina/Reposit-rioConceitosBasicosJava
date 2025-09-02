4cd037ff1ecbba4b446e790e2e1b8da898e322c2
lucasdellagiustina <161031319+lucasdellagiustina@users.noreply.github.com>
2025-09-02 18:33:58 -0300

    Update TiposPrimitivos.java

--- entradadedados/TiposPrimitivos.java
+++ entradadedados/TiposPrimitivos.java
@@ -8,9 +8,10 @@ public class TiposPrimitivos {
         String nomeAluno = teclado.nextLine();
         System.out.printf("Digite a nota de %s: ", nomeAluno);
         float nota = teclado.nextFloat();
-        System.out.printf("A nota de %s é %.2f%n", nomeAluno, nota); // Exemplo: Paulo, 8;50
+        System.out.printf("A nota de %s é %.2f%n", nomeAluno, nota); // Exemplo: Paulo, 8.50
         teclado.close(); //Mesma coisa do somador de números, necessário fechar o scanner
     }
 }    
+
 
 
