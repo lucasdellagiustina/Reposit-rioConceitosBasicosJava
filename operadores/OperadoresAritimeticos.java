c429d2e5b60ada1e2c5f006ee52929650983baf5
lucasdellagiustina <161031319+lucasdellagiustina@users.noreply.github.com>
2025-09-02 17:20:49 -0300

    operadores/OperadoresAritimeticos.java

---
+++ operadores/OperadoresAritimeticos.java
@@ -0,0 +1,10 @@
+package operadores;
+
+public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+        int nota1 = 3;
+        int nota2 = 10;
+        float mediaNotas = (nota1 + nota2) / 2;
+        System.out.printf("Sua nota é %.1f \n", mediaNotas);
+    }
+}
